8664d71690a18543975b71dcda2d5d841dc5f697
reuben frith <reubenfrith007@gmail.com>
2023-02-22 22:06:41 +1100

    fin setup

---
+++ src/components/Gif.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  handleClick = () => {
+  }
+
+  render() {
+    const src = `https://media2.giphy.com/media/${this.props.id}/200.gif`;
+    return (
+      <img src={src} className="gif" onClick={this.handleClick} />
+    );
+  }
+}
+
+export default Gif;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+
+ 
+
+  render() {
+
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default SearchBar;
