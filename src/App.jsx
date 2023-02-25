3e8a71b8b205e789ecca06d4225b40b2763a535d
reuben frith <reubenfrith007@gmail.com>
2023-02-25 11:15:08 +1100

    completed styling

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,14 @@ const App = () => {
   // const [count, setCount] = useState(0)
 
   const gifs = [
-    { id: "xT9IgDEI1iZyb2wqo8" },
-    { id: "3o6ZtaO9BZHcOjmErm" },
-    { id: "3o6Zt5jXZdVJiN1eR6" },
-    { id: "3o6Zt6Uz0ZdX3RdKVG" }
+    { id: 'xT9IgDEI1iZyb2wqo8' },
+    { id: '3o6ZtaO9BZHcOjmErm' },
+    { id: '3o6Zt5jXZdVJiN1eR6' },
+    { id: '3o6Zt6Uz0ZdX3RdKVG' }
   ]
 
   return (
-    <div>
+    <div id="app">
     <div className="left-scene">
       <SearchBar />
       <div className="selected-gif">
